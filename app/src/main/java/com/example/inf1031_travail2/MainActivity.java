78f0d2fd99123dc4f99bdd087ea88d81446e86d7
ismaelcoulibaly <ismael.coulibaly99@gmail.com>
2021-11-11 13:55:45 -0500

    ajustement getDonnes() + onOnclick()

--- app/src/main/java/com/example/inf1031_travail2/MainActivity.java
+++ app/src/main/java/com/example/inf1031_travail2/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.inf1031_travail2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.UserDictionary;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -72,6 +75,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        switch(v.getId()) {
+
+            case R.id.ajouter_btn:
+                //Pour insérer des données dans le dictionnaire
+                //Defines a new Uri object that receives the result of the insertion
+                Uri newUri;
+
+                ContentValues newValues = new ContentValues();
+
+                newValues.put(UserDictionary.Words.APP_ID, appIdEditTxt.getText().toString());
+                newValues.put(UserDictionary.Words.LOCALE, localeEditTxt.getText().toString());
+                newValues.put(UserDictionary.Words.WORD, wordEditTxt.getText().toString());
+                newValues.put(UserDictionary.Words.FREQUENCY, frequencyEditTxt.getText().toString());
+
+                newUri = getContentResolver().insert(
+                        UserDictionary.Words.CONTENT_URI,   // the user dictionary content URI
+                        newValues                          // the values to insert
+                );
+                Toast.makeText(this, "Uri = " + newUri, Toast.LENGTH_LONG).show();
+
+                //To reload the activity
+                finish();
+                startActivity(getIntent());
+                break;
+
+
+            case R.id.supprimer_ligne_btn:
+                //Pour delete les données dans le dictionnaire
+
+                String selection = UserDictionary.Words.FREQUENCY + " = ?";
+                String[] selectionArgs = {"200"} ;
+
+                int numberDeleted = 0;
+                numberDeleted = getContentResolver().delete(
+                        UserDictionary.Words.CONTENT_URI,selection, selectionArgs);
+
+                Toast.makeText(this, "row matching deleted = " + numberDeleted, Toast.LENGTH_LONG).show();
+                finish();
+                startActivity(getIntent());
+                break;
+
+        }
+
 
     }
 }
